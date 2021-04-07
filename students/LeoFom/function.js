ea499c4fae0bb74dd013b535d62aa4e73fd54642
LeoFom <l.fomenko003@gmail.com>
2021-04-07 18:00:59 +0300

    Hi

---
+++ students/LeoFom/function.js
@@ -0,0 +1,6 @@
+// el=$('#st').hide();
+// console.log(el)
+var butt=$('#mybutton');
+butt.on('click',function(){
+    $('#mycontent').html('New string');
+});
---
+++ students/LeoFom/yes/content/index.md
@@ -0,0 +1,5 @@
+Title: My title Hello!
+Date: 2010-10-03 10:20
+Category: Reviev
+
+HEllo
