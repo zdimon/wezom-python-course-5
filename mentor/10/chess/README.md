a7cd0f877def996fe170c3294a25c51e9d701e4f
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-21 18:36:04 +0300

    commit from zdimon77@gmail.com ----

---
+++ mentor/10/chess/README.md
@@ -0,0 +1,14 @@
+board = [
+    [ {
+         "color": "black|white",
+         "active": "true|false"
+         "figure": {
+             "image": "1.png",
+             "helth": "100%"
+         }   
+      }
+        ....
+    ]
+    []
+   ....
+]
--- mentor/10/chess/backend/chess/views.py
+++ mentor/10/chess/backend/chess/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from backend.settings import BASE_DIR
+import os
+
+def create_board_json():
+    path = os.path.join(BASE_DIR,'static', 'board.json')
+    if not os.path.isfile(path):
+        with open(path, 'w') as f:
+            f.write('asdasdas')
+
+        
 
 def index(request):
+    create_board_json()
     return render(request,'index.html',{})
