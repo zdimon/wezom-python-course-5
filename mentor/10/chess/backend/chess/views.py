f376e626cc0cdcc463959ac4e629d6ebc988ccd6
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-21 19:15:44 +0300

    commit from zdimon77@gmail.com ----

--- mentor/10/chess/backend/chess/views.py
+++ mentor/10/chess/backend/chess/views.py
@@ -9,26 +9,24 @@ import json
 
 def make_board():
     board = []
-    for i in range(0,7):
-        board.append({})
+    for i in range(0,8):
+        tmp = []
+        for j in range(0,8):
+            tmp.append({"color": "black"})
+        board.append(tmp)
+
+    return board
 
 def create_board_json():
-    board = [ {
-         "color": "black|white",
-         "active": "true|false",
-         "figure": {
-             "image": "1.png",
-             "helth": "100%"
-         }   
-      }      
-    ]
+    board = make_board()
     path = os.path.join(BASE_DIR,'static', 'board.json')
-    if not os.path.isfile(path):
-        with open(path, 'w') as f:
-            f.write(json.dumps(board))
+    #if not os.path.isfile(path):
+    with open(path, 'w') as f:
+        f.write(json.dumps(board))
+    return board
 
         
 
 def index(request):
-    create_board_json()
-    return render(request,'index.html',{})
+    board = create_board_json()
+    return render(request,'index.html',{"board": board})
