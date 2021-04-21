86d35b1cb242198ea1d821d8a3df66e863df6e82
Dmitry Zharikov <zdimon77@gmail.com>
2021-04-21 18:42:21 +0300

    commit from zdimon77@gmail.com ----

--- mentor/10/chess/backend/chess/views.py
+++ mentor/10/chess/backend/chess/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from backend.settings import BASE_DIR
 import os
+import json
+
+# d = json.loads('{"ss": "dsdas"}')
+# f = {}
+# f = json.dumps(f)
+
+def make_board():
+    board = []
+    for i in range(0,7):
+        board.append({})
 
 def create_board_json():
+    board = [ {
+         "color": "black|white",
+         "active": "true|false",
+         "figure": {
+             "image": "1.png",
+             "helth": "100%"
+         }   
+      }      
+    ]
     path = os.path.join(BASE_DIR,'static', 'board.json')
     if not os.path.isfile(path):
         with open(path, 'w') as f:
-            f.write('asdasdas')
+            f.write(json.dumps(board))
 
         
 
